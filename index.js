4f4b07c98acca6e8690808aedd7e49dfaba442d1
Mark <mark.dewit@xillio.nl>
2018-01-03 11:58:04 +0100

    Read data with Node.js and print it

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello from Nodejs');
+var fs = require ('fs');
+var meat;
+
+fs.readFile('tmp/meat.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    meat = JSON.parse(data);
+    console.log(meat);
+});
